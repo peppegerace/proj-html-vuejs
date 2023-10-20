063c5e567e91e212d1b0443ccf7f322f3a8c8c5f
peppegerace <peppegerace47@gmail.com>
2023-10-20 15:58:41 +0200

    [add] static structure Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,10 +6,154 @@ export default {
 
 
 <template>
-  <h1>Header</h1>
+  
+  <header>
+    <div class="top-header">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <span><i class="fa-solid fa-envelope"></i>info@example.com</span>
+            <span><i class="fa-solid fa-phone"></i>+1(817)901 3377</span>
+          </div>
+          <div class="col">
+            <nav>
+              <ul>
+                <li>
+                  <a href="#">
+                    <i class="fa-brands fa-square-facebook"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa-brands fa-square-pinterest"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa-brands fa-square-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa-brands fa-linkedin"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa-brands fa-square-instagram"></i>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="bottom-header">
+      <div class="container">
+        <div class="row ">
+          <div class="col">
+            <img src="/logo.png" alt="">
+          </div>
+          <div class="col">
+            <nav>
+              <ul>
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">About</a>
+                </li>
+                <li>
+                  <a href="#">Services</a>
+                </li>
+                <li>
+                  <a href="#">Training</a>
+                </li>
+                <li>
+                  <a href="#">Blogs</a>
+                </li>
+              </ul>
+              <button class="btn-custom">Get In Toch!</button>
+            </nav>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </header>
+
 </template>
 
 
-<style>
+<style lang="scss" scoped>
+@use '../scss/main.scss' as *;
 
+.container {
+    height: 100%;
+  }
+.row {
+  height: 100%;
+  justify-content: space-between;
+  align-items: center;
+  }
+
+.top-header {
+  background-color: $green;
+  height: 50px;
+  color: $whitesmoke;
+  font-size: 0.9rem;
+  span {
+    margin-right: 30px;
+    i {
+      color: $orange;
+      margin-right: 10px;
+    }
+  }
+  nav {
+    display: flex;
+    height: 100%;
+    align-items: center;
+    li {
+      display: inline-block;
+      font-size: 1.4rem;
+      margin-right: 10px;
+    }
+  }
+}
+
+.bottom-header {
+  height: 100px;
+
+  nav {
+    display: flex;
+    height: 100%;
+    align-items: center;
+    font-size: 1.3rem;
+  }
+
+  li {
+    display: inline-block;
+    margin-right: 40px;
+
+    a {
+      color: grey;
+      &:hover {
+        color: $orange;
+      }
+    }
+  }
+
+  .btn-custom {
+    padding: 15px 30px;
+    background-color: $orange;
+    border-radius: 25px;
+    cursor: pointer;
+    border: none;
+    color: $white;
+    font-size: 1.3rem;
+  }
+}
 </style>
